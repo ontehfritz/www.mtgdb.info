9afd786a1b2a6298cbe3dc8d98a05116eaaa627c
fritzcoder <fredrick.seitz@gmail.com>
2014-01-24 21:03:19 +0100

    update

--- src/www.mtgdb.info/www.mtgdb.info/Modules/SearchModule.cs
+++ src/www.mtgdb.info/www.mtgdb.info/Modules/SearchModule.cs
@@ -2,6 +2,7 @@ using System;
 using Nancy;
 using Nancy.ModelBinding;
 using MtgDb.Info.Driver;
+using System.Linq;
 
 namespace MtgDb.Info
 {
@@ -20,6 +21,10 @@ namespace MtgDb.Info
             Post ["/search"] = parameters => {
                 SearchModel model = this.Bind<SearchModel>();
                 model.Cards = magicdb.Search(model.Term);
+                model.Cards = model.Cards
+                    .AsEnumerable()
+                    .OrderBy(x => x.Name)
+                    .ThenByDescending(x => x.ReleasedAt).ToArray();
                
                 return View["Search", model];
             };
