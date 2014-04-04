926c2e39de6117f702faaeaee5d601b309540ea9
fritzcoder <fredrick.seitz@gmail.com>
2014-04-04 19:38:40 +0200

    yep, more code

---
+++ src/www.mtgdb.info/www.mtgdb.info/Models/Deck.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MtgDb.Info
+{
+    public class Deck
+    {
+        public Guid UserId          { get; set; }
+        public string Name          { get; set; }
+        public bool IsPublic        { get; set; }
+        public List<DeckCard> Cards { get; set; }
+        public DateTime CreatedAt   { get; set; }
+        public DateTime ModifiedAt  { get; set; }
+
+        public Deck ()
+        {
+            Cards = new List<DeckCard>();
+        }
+    }
+
+    public class DeckCard
+    {
+        public int MultiverseId { get; set; }
+        public int Amount       { get; set; }
+    }
+}
+
---
+++ src/www.mtgdb.info/www.mtgdb.info/Models/IDeckRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MtgDb.Info
+{
+    public class IDeckRepository
+    {
+        public IDeckRepository ()
+        {
+        }
+    }
+}
+
