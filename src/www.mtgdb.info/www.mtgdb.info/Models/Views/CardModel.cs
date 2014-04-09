dcf218de7676445e4b4323c610143a7e3a7b08b1
fritzcoder <fredrick.seitz@gmail.com>
2014-04-09 17:23:03 +0200

    set.Total

--- src/www.mtgdb.info/www.mtgdb.info/Models/Views/CardModel.cs
+++ src/www.mtgdb.info/www.mtgdb.info/Models/Views/CardModel.cs
@@ -5,6 +5,7 @@ namespace MtgDb.Info
     public class CardModel : PageModel
     {
         public Card Card        { get; set; }
+        public CardSet Set      { get; set; }
         public int Amount       { get; set; } 
         public int Page         { get; set; }
         public Card[] Prints    { get; set; }
