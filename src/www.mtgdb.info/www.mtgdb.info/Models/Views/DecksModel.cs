7fd51b930644da67d5e353625ac866626a0ed5d8
fritzcoder <fredrick.seitz@gmail.com>
2014-04-17 17:48:41 +0200

    some deck stuff

---
+++ src/www.mtgdb.info/www.mtgdb.info/Models/Views/DecksModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MtgDb.Info
+{
+    public class DecksModel : PageModel
+    {
+        List<Deck> Decks { get; set; }
+
+        public DecksModel () : base()
+        {
+            Decks = new List<Deck>();
+        }
+    }
+}
+
